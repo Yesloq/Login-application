d9ae6910e7279654f3893fe3eca407711fbf8c17
Yesid Lourido <yesidlourido@gmail.com>
2022-11-05 08:19:26 -0500

    Completo con diccionarios

--- main.py
+++ main.py
@@ -1,152 +1,276 @@
 from datetime import datetime
+data_users = []
 
-def val_mnumber():
-    mnumber = input('\nPlease enter your mobile number: ')
-    val = False
-    while val == False:
+#This function validates mobile number
+def validate_mobile_number(mobile_number):
+    is_valid = False
+    
+    while is_valid == False:
         try:
-            if mnumber.isnumeric() == False:
-                raise TypeError('The entered value is not a number')
-                
+            if mobile_number.isnumeric() == False:
+                raise TypeError('The entered value is not a number\n')
             
-            elif len(mnumber) != 10:
-                raise ValueError('Enter a 10 digit number')
-                
+            elif len(mobile_number) != 10:
+                raise ValueError('Enter a 10 digit number\n')
             
-            elif mnumber[0] != '0':
-                raise NameError('The cell number must begin with 0')
-                
+            elif mobile_number[0] != '0':
+                raise NameError('The cell number must begin with 0\n')
             
             else:
-                #print('The mobile number is correct.')
-                val = True
+                is_valid = True
 
         except TypeError as TE:
             print(TE)
-            mnumber = input('\nPlease enter your mobile number: ')
+            mobile_number = input('Please enter your mobile number: ')
         
         except ValueError as VE:
             print(VE)
-            mnumber = input('\nPlease enter your mobile number: ')
+            mobile_number = input('Please enter your mobile number: ')
         
         except NameError as NE:
             print(NE)
-            mnumber = input('\nPlease enter your mobile number: ')
+            mobile_number = input('Please enter your mobile number: ')
     
-    return mnumber
+    return mobile_number
 
-def val_character(password):
+#This function validates if password contains an @ or $
+def validate_special_character(password):
     if '@' in password or '&' in password:
-        val = True
-    else:
-        val = False
+        is_valid = True
     
-    return val
+    else:
+        is_valid = False
+    
+    return is_valid
 
-def val_alphabetic(password):
+#This function validates if first character is an alphabetic one
+def validate_alphabetic(password):
     if password[0].isalpha():
-        val = True
+        is_valid = True
+    
     else:
-        val = False
+        is_valid = False
     
-    return val
+    return is_valid
 
-def val_password():
-    password = input('\nPlease enter your password: ')
-    confirm_password = input('\nPlease confirm your password: ')
-    val = False
+#This function validates if last password character is numerical
+def validate_last_character(password):
+    if password[len(password) - 1].isnumeric():
+        is_valid = True
     
-    while val == False:
+    else:
+        is_valid = False
+    
+    return is_valid
+
+#This function validates passwords
+def validate_password(password, confirm_password):
+    is_valid = False
+    
+    while is_valid == False:
         try:
-            s_character = val_character(password)
-            s_alphabetic = val_alphabetic(password)
-            s_numeric = password[len(password) - 1].isnumeric()
+            s_character = validate_special_character(password)
+            s_alphabetic = validate_alphabetic(password)
+            s_numeric = validate_last_character(password)
             
-            if s_character == False:
-                raise ValueError('The password must have a @ or an &.\nPlease start again')
+            if password != confirm_password:
+                raise NameError('Your passwords are not matching.\nPlease start again:\n')
+
+            elif s_character == False:
+                raise ValueError('The password must have a @ or an &.\nPlease start again\n')
             
             elif s_alphabetic == False:
-                raise AttributeError('The first character of the password must be a letter.\nPlease start again')
+                raise AttributeError('The first character of the password must be a letter.\nPlease start again\n')
             
             elif s_numeric == False:
-                raise TypeError('The last element of the password must be a number')
+                raise TypeError('The last element of the password must be a number\n')
 
-            elif password != confirm_password:
-                raise NameError('Your passwords are not matching.\nPlease start again:\n')
-            
             else:
-                #print('\nYour password is correct.')
-                val = True
+                is_valid = True
         
+        except NameError as NE:
+            print(NE)
+            password = input('Please enter your password: ')
+            confirm_password = input('\nPlease confirm your password: ')
+
         except TypeError as TE:
             print(TE)
-            password = input('\nPlease enter your password: ')
+            password = input('Please enter your password: ')
             confirm_password = input('\nPlease confirm your password: ')
         
         except AttributeError as AE:
             print(AE)
-            password = input('\nPlease enter your password: ')
-            confirm_password = input('\nPlease confirm your password: ')
-
-        except NameError as NE:
-            print(NE)
-            password = input('\nPlease enter your password: ')
+            password = input('Please enter your password: ')
             confirm_password = input('\nPlease confirm your password: ')
         
         except ValueError as VE:
             print(VE)
-            password = input('\nPlease enter your password: ')
+            password = input('Please enter your password: ')
             confirm_password = input('\nPlease confirm your password: ')
     
     return password
 
-def val_date():
-    date = input('\nPlease Enter your Date of Birth # DD/MM/YYYY (No Space): ')
-    val = False
+#This function validates date of birth
+def validate_date(date):
+    is_valid = False
 
-    while val == False:
+    while is_valid == False:
         try:
             datetime.strptime(date, '%d/%m/%Y')
-            val = True
+            is_valid = True
+        
         except ValueError:
-            print('The date format is DD/MM/YYYY')
-            date = input('\nPlease Enter your Date of Birth # DD/MM/YYYY (No Space): ')
+            print('The date format is DD/MM/YYYY\n')
+            date = input('Please Enter your Date of Birth # DD/MM/YYYY (No Space): ')
     
-    print()
-    #print('The date is correct')
     return date
 
-data_users = []
+#This funtion validates sign in information
+def validate_signin(username, password):
+    no_user_found = True
 
-def signup(user):
+    for user in data_users:
+        
+        if user["mobile_number"] == username:
+
+            if user["password"] == password:
+                print('You have Successfully Signed in.\nWelcome ' + user["name"] + '\n')
+                return user
+
+            else:
+                print('You have entered the wrong Password.\nPlease try againg\n')
+                return any
+    
+    if no_user_found:
+        print('You have not Signed up with this Contact Number, Please Sign Up first\n')
+        return any
+
+#This function validates if old password match with the saved user password   
+def validate_user_password(user):
+    username = input('\nPlease enter your Username (Mobile number): ')
+    old_password = input('\nPlease enter your old password: ')
+    
+    if user["password"] == old_password:
+        return True
+    
+    else:
+        print("You have entered the wrong Password.\nPlease try again\n")
+        return False
+
+#This function allow to reset password when maximum attemp is over
+def validate_max_attemp_user(user):
+    username = input('\nPlease enter your Username (Mobile number) to confirm: ')
+
+    if user["mobile_number"] == username:
+        date_of_birth = input('\nPlease Enter your Date of Birth # DD/MM/YYYY (No Space): ')
+
+        if user["date_of_birth"] == date_of_birth:
+            new_password = input('\nPlease enter your new password: ')
+            confirm_new_password = input('\nPlease enter your new password: ')
+            validate_password(new_password,confirm_new_password)
+
+            if user["password"] != new_password:
+                user["password"] = new_password
+                return user
+            
+            else:
+                print("You cannot use the password used earlier.\n")
+                return any
+        
+        else:
+            print("You cannot use the password used earlier.\n")
+            return any
+    else:
+        return any
+
+#This function allow to reset user password  
+def reset_password(user):
+    attemp = 0
+    is_valid = False
+
+    while is_valid == False:
+        valid_user = validate_user_password(user)
+
+        if attemp == 2:
+            print("You have used the maximum attemps of login:\nPlease reset the password by entering the below details:\n")
+            return validate_max_attemp_user(user)
+            
+        if valid_user:
+            new_password = input('\nPlease enter your new password: ')
+            
+            if user["password"] != new_password:
+                confirm_new_password = input('\nPlease enter your new password: ')
+                validate_password(new_password,confirm_new_password)
+
+                user["password"] = new_password
+                is_valid = True
+
+            else:
+                print("You cannot use the password used earlier.\nPlease try again")
+                attemp += 1
+
+        else:
+            attemp += 1
+            
+def signin_menu(user):
+    select_signin = input('Please Enter 1 for Resetting the Password.\nPlease Enter 2 for Signout.\n')
+    while select_signin != '2':
+        
+        if select_signin == '1':
+            if reset_password(user) != any:
+                select_signin = '0'
+            else:
+                return
+        else:
+            select_signin = input('Please Enter 1 for Resetting the Password.\nPlease Enter 2 for Signout.\n')
+
+    return
+
+def signup():
     name = input('\nPlease enter your full name: ')
-    mobile_number = val_mnumber()
-    password = val_password()
-    dob = val_date()
 
-    data_users.insert(user, {
+    mobile_number = input('\nPlease enter your mobile number: ')
+    validate_mobile_number(mobile_number)
+    
+    password = input('\nPlease enter your password: ')
+    confirm_password = input('\nPlease confirm your password: ')
+    validate_password(password, confirm_password)
+    
+    date_of_birth = input('\nPlease Enter your Date of Birth # DD/MM/YYYY (No Space): ')
+    validate_date(date_of_birth)
+    age = 2022 - int(date_of_birth[-4:])
+
+    if age < 21:
+        print('Only users over 21 years of age can register.\n')
+        return
+
+    data_users.append({
         "name": name,
         "mobile_number": mobile_number,
         "password": password,
-        "dob": dob
+        "date_of_birth": date_of_birth
     })
 
-    print('\nYou have Successfully Signed up.')
+    print('You have Successfully Signed up.\n')
     
     return data_users
 
 def signin():
-    print('Sign in!')
+    user_name = input('\nPlease enter your username (Mobile Number): ')
+    password = input('\nPlease enter your password: ')
+    valid_user = validate_signin(user_name, password)
+    
+    if valid_user != any:
+        signin_menu(valid_user)
+        return
 
 def run():
-    user = 0 #Esta variable permite saber la posicion dentro de la lista del usuario registrado
     select = input('Please Enter 1 for Sign up.\nPlease Enter 2 for Sign in.\nPlease Enter 3 for Quit.\n')
 
     while select != '3':
         
         if select == '1':
-            print(signup(user))
-            user += 1
+            signup()
             select = '0'
         
         elif select == '2':
@@ -158,7 +282,6 @@ def run():
     
     print('Thank You for using the Application')
     
-
 
 if __name__ == '__main__':
     run()
